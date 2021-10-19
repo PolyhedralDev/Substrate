ee97f1fde7fee0924b41d3e54d52632df7c81979
dfsek <dfsek@protonmail.com>
2021-10-19 10:55:40 -0700

    implement dummy BlockNode

---
+++ src/main/java/com/dfsek/substrate/lang/node/BlockNode.java
@@ -0,0 +1,36 @@
+package com.dfsek.substrate.lang.node;
+
+import com.dfsek.substrate.lang.internal.BuildData;
+import com.dfsek.substrate.lang.internal.Node;
+import com.dfsek.substrate.parser.exception.ParseException;
+import com.dfsek.substrate.tokenizer.Position;
+import org.objectweb.asm.MethodVisitor;
+
+import java.util.List;
+
+public class BlockNode implements Node {
+    private final List<Node> contents;
+    private final Position position;
+
+    public BlockNode(List<Node> contents, Position position) {
+        this.contents = contents;
+        this.position = position;
+    }
+
+    @Override
+    public void apply(MethodVisitor visitor, BuildData data) throws ParseException {
+        contents.forEach(node -> node.apply(visitor, data));
+    }
+
+    @Override
+    public Position getPosition() {
+        return position;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("{\n");
+        contents.forEach(node -> builder.append(node).append('\n'));
+        return builder.append('}').toString();
+    }
+}
--- src/main/java/com/dfsek/substrate/lang/rules/BlockRule.java
+++ src/main/java/com/dfsek/substrate/lang/rules/BlockRule.java
@@ -2,13 +2,16 @@ package com.dfsek.substrate.lang.rules;
 
 import com.dfsek.substrate.lang.Rule;
 import com.dfsek.substrate.lang.internal.Node;
+import com.dfsek.substrate.lang.node.BlockNode;
 import com.dfsek.substrate.parser.Parser;
 import com.dfsek.substrate.parser.exception.ParseException;
 import com.dfsek.substrate.tokenizer.Tokenizer;
 
+import java.util.Collections;
+
 public class BlockRule implements Rule {
     @Override
     public Node assemble(Tokenizer tokenizer, Parser parser) throws ParseException {
-        return null;
+        return new BlockNode(Collections.emptyList(), tokenizer.consume().getPosition());
     }
 }
