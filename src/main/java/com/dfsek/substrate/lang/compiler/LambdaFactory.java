9d89d3bc6e5d2a2f09f2bd17acddbdb82483c8f0
dfsek <dfsek@protonmail.com>
2021-10-19 22:54:29 -0700

    lambda function generation

--- src/main/java/com/dfsek/substrate/lang/compiler/LambdaFactory.java
+++ src/main/java/com/dfsek/substrate/lang/compiler/LambdaFactory.java
@@ -2,6 +2,7 @@ package com.dfsek.substrate.lang.compiler;
 
 import com.dfsek.substrate.parser.DynamicClassLoader;
 import com.dfsek.substrate.util.ReflectionUtil;
+import com.dfsek.substrate.util.pair.ImmutablePair;
 import org.apache.commons.io.IOUtils;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
@@ -9,13 +10,16 @@ import org.objectweb.asm.MethodVisitor;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.BiConsumer;
 
 import static org.objectweb.asm.Opcodes.*;
 
 public class LambdaFactory {
-    private final Map<Signature, Map<Signature, Class<?>>> generated = new HashMap<>();
+    private final Map<Signature, Map<Signature, ImmutablePair<Class<?>, List<Class<?>>>>> generated = new HashMap<>();
 
     private final DynamicClassLoader classLoader;
     private final TupleFactory tupleFactory;
@@ -65,7 +69,74 @@ public class LambdaFactory {
                 }
             }
 
-            return classLoader.defineClass(name.replace('/', '.'), writer.toByteArray());
-        });
+            return ImmutablePair.of(classLoader.defineClass(name.replace('/', '.'), writer.toByteArray()), new ArrayList<>());
+        }).getLeft();
+    }
+
+    public Class<?> implement(Signature args, Signature returnType, BiConsumer<MethodVisitor, ClassWriter> consumer) {
+        Class<?> iface = generate(args, returnType);
+
+        ImmutablePair<Class<?>, List<Class<?>>> pair = generated.get(args).get(returnType);
+
+        ClassWriter writer = new ClassWriter(org.objectweb.asm.ClassWriter.COMPUTE_FRAMES + org.objectweb.asm.ClassWriter.COMPUTE_MAXS);
+        String name = "com/dfsek/substrate/lang/internal/tuple/LambdaIMPL" + args.classDescriptor() + "$" + returnType.classDescriptor() + "$" + pair.getRight().size();
+
+        writer.visit(V1_8,
+                ACC_PUBLIC,
+                name,
+                null,
+                "java/lang/Object",
+                new String[]{ReflectionUtil.internalName(pair.getLeft())});
+
+        String constructorSig = "(" + args.internalDescriptor() + ")V";
+
+        MethodVisitor constructor = writer.visitMethod(ACC_PUBLIC,
+                "<init>", // Constructor method name is <init>
+                constructorSig,
+                null,
+                null);
+
+        constructor.visitCode();
+        constructor.visitVarInsn(ALOAD, 0); // Put this reference on stack
+        constructor.visitMethodInsn(INVOKESPECIAL, // Invoke Object super constructor
+                "java/lang/Object",
+                "<init>",
+                "()V",
+                false);
+
+        constructor.visitInsn(RETURN); // Void return
+        constructor.visitMaxs(0, 0); // Set stack and local variable size (bogus values; handled automatically by ASM)
+
+        String ret = returnType.internalDescriptor();
+
+        if(!returnType.isSimple()) {
+            ret = "L" + ReflectionUtil.internalName(tupleFactory.generate(returnType)) + ";";
+        }
+
+        MethodVisitor apply = writer.visitMethod(ACC_PUBLIC,
+                "apply",
+                "(" + args.internalDescriptor() + ")" + ret,
+                null,
+                null);
+
+        consumer.accept(apply, writer);
+        apply.visitMaxs(0, 0);
+
+        byte[] bytes = writer.toByteArray();
+
+        if(true) {
+            File dump = new File("./dumps/" + name + ".class");
+            dump.getParentFile().mkdirs();
+            System.out.println("Dumping to " + dump.getAbsolutePath());
+            try {
+                IOUtils.write(bytes, new FileOutputStream(dump));
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Class<?> clazz = classLoader.defineClass(name.replace('/', '.'), writer.toByteArray());
+        pair.getRight().add(clazz);
+        return clazz;
     }
 }
