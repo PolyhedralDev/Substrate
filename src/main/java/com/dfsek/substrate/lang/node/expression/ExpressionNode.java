74c8017d1e3cd1ef91e6879541fcb55c6a4a1538
dfsek <dfsek@protonmail.com>
2021-10-19 19:09:55 -0700

    make ExpressionNode abstract

--- src/main/java/com/dfsek/substrate/lang/node/expression/ExpressionNode.java
+++ src/main/java/com/dfsek/substrate/lang/node/expression/ExpressionNode.java
@@ -1,19 +1,6 @@
 package com.dfsek.substrate.lang.node.expression;
 
 import com.dfsek.substrate.lang.Node;
-import com.dfsek.substrate.lang.compiler.BuildData;
-import com.dfsek.substrate.parser.exception.ParseException;
-import com.dfsek.substrate.tokenizer.Position;
-import org.objectweb.asm.MethodVisitor;
 
-public class ExpressionNode implements Node {
-    @Override
-    public void apply(MethodVisitor visitor, BuildData data) throws ParseException {
-
-    }
-
-    @Override
-    public Position getPosition() {
-        return null;
-    }
+public abstract class ExpressionNode implements Node {
 }
