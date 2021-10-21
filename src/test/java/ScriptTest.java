5b47a27989950ac990b10a7d38b24534e8364e42
dfsek <dfsek@protonmail.com>
2021-10-21 00:24:52 -0700

    add invalid tests

--- src/test/java/ScriptTest.java
+++ src/test/java/ScriptTest.java
@@ -13,46 +13,75 @@ import org.objectweb.asm.Opcodes;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+import static org.junit.Assert.fail;
+
 public class ScriptTest {
-    public void script(String file) throws IOException, ParseException {
-        String data = IOUtils.toString(ScriptTest.class.getResource(file), StandardCharsets.UTF_8);
-        Parser parser = new Parser(data, new BaseRule());
-        parser.parse().execute(null);
+    public void script(String file) {
+        try {
+            String data = IOUtils.toString(ScriptTest.class.getResource(file), StandardCharsets.UTF_8);
+            Parser parser = new Parser(data, new BaseRule());
+            parser.parse().execute(null);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void invalidScript(String file) {
+        try {
+            String data = IOUtils.toString(ScriptTest.class.getResource(file), StandardCharsets.UTF_8);
+            Parser parser = new Parser(data, new BaseRule());
+            parser.parse().execute(null);
+        } catch (ParseException e) {
+            return; // These scripts should fail to parse
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        fail(); // If it parsed, something is wrong.
     }
 
     @Test
-    public void script() throws IOException {
+    public void script() {
         script("/valid/test.sbsc");
     }
 
     @Test
-    public void tuple() throws IOException {
+    public void tuple() {
         script("/valid/tuple.sbsc");
     }
 
     @Test
-    public void scope() throws IOException {
+    public void scope() {
         script("/valid/scope.sbsc");
     }
 
     @Test
-    public void functionSimple() throws IOException {
+    public void functionSimple() {
         script("/valid/functionSimple.sbsc");
     }
 
     @Test
-    public void functionNesting() throws IOException {
+    public void functionNesting() {
         script("/valid/functionNesting.sbsc");
     }
 
     @Test
-    public void functionReturn() throws IOException {
+    public void functionReturn() {
         script("/valid/functionReturn.sbsc");
     }
 
     @Test
-    public void functionTuple() throws IOException {
+    public void functionTuple() {
         script("/valid/functionTuple.sbsc");
+    }
+
+    @Test
+    public void noStatementEnd() {
+        invalidScript("/invalid/noStatementEnd.sbsc");
+    }
+
+    @Test
+    public void danglingCloseBrace() {
+        invalidScript("/invalid/danglingCloseBrace.sbsc");
     }
 
     @Test
