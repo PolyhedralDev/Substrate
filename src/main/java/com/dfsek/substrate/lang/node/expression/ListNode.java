906bad97079e1bcd433d5645177c66c627fc7233
dfsek <dfsek@protonmail.com>
2021-10-25 23:17:24 -0700

    ListNode parameters -> elements

--- src/main/java/com/dfsek/substrate/lang/node/expression/ListNode.java
+++ src/main/java/com/dfsek/substrate/lang/node/expression/ListNode.java
@@ -11,30 +11,30 @@ import org.objectweb.asm.MethodVisitor;
 import java.util.List;
 
 public class ListNode extends ExpressionNode {
-    private final List<ExpressionNode> args;
+    private final List<ExpressionNode> elements;
     private final Position position;
 
-    public ListNode(List<ExpressionNode> args, Position position) {
-        this.args = args;
+    public ListNode(List<ExpressionNode> elements, Position position) {
+        this.elements = elements;
         this.position = position;
     }
 
     @Override
     public void apply(MethodVisitor visitor, BuildData data) throws ParseException {
-        Signature signature = args.get(0).referenceType(data);
-        args.forEach(arg -> {
+        Signature signature = elements.get(0).referenceType(data);
+        elements.forEach(arg -> {
             if(!arg.referenceType(data).equals(signature)) {
                 throw new ParseException("Array element mismatch. Expected " + signature + ", got " + arg.referenceType(data), position);
             }
         });
 
-        CompilerUtil.pushInt(args.size(), visitor);
-        Signature params = args.get(0).referenceType(data);
+        CompilerUtil.pushInt(elements.size(), visitor);
+        Signature params = elements.get(0).referenceType(data);
         params.getType(0).applyNewArray(visitor, params.getGenericReturn(0));
-        for (int i = 0; i < args.size(); i++) {
+        for (int i = 0; i < elements.size(); i++) {
             visitor.visitInsn(DUP); // duplicate reference for all elements.
             CompilerUtil.pushInt(i, visitor); // push index
-            args.get(i).applyReferential(visitor, data); // apply value
+            elements.get(i).applyReferential(visitor, data); // apply value
             visitor.visitInsn(params.getType(0).arrayStoreInsn());
         }
     }
@@ -46,6 +46,6 @@ public class ListNode extends ExpressionNode {
 
     @Override
     public Signature returnType(BuildData data) {
-        return new Signature(DataType.LIST).applyGenericReturn(0, args.get(0).returnType(data));
+        return new Signature(DataType.LIST).applyGenericReturn(0, elements.get(0).returnType(data));
     }
 }
