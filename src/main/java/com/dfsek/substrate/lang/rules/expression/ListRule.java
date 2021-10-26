6aa0aa22344d9055ce5836a9d839f352334ebc0a
dfsek <dfsek@protonmail.com>
2021-10-25 22:21:13 -0700

    implement ListRule

--- src/main/java/com/dfsek/substrate/lang/rules/expression/ListRule.java
+++ src/main/java/com/dfsek/substrate/lang/rules/expression/ListRule.java
@@ -3,17 +3,34 @@ package com.dfsek.substrate.lang.rules.expression;
 import com.dfsek.substrate.lang.Node;
 import com.dfsek.substrate.lang.Rule;
 import com.dfsek.substrate.lang.node.expression.ExpressionNode;
+import com.dfsek.substrate.lang.node.expression.ListNode;
 import com.dfsek.substrate.parser.ParserUtil;
 import com.dfsek.substrate.parser.exception.ParseException;
 import com.dfsek.substrate.tokenizer.Token;
 import com.dfsek.substrate.tokenizer.Tokenizer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListRule implements Rule {
     private static final ListRule INSTANCE = new ListRule();
     @Override
     public ExpressionNode assemble(Tokenizer tokenizer) throws ParseException {
-        ParserUtil.checkType(tokenizer.consume(), Token.Type.LIST_BEGIN);
-        return null;
+        Token op = ParserUtil.checkType(tokenizer.consume(), Token.Type.LIST_BEGIN);
+
+        List<ExpressionNode> elements = new ArrayList<>();
+
+        while(tokenizer.peek().getType() != Token.Type.LIST_END) {
+            elements.add(ExpressionRule.getInstance().assemble(tokenizer));
+            if (ParserUtil.checkType(tokenizer.peek(), Token.Type.SEPARATOR, Token.Type.LIST_END, Token.Type.STATEMENT_END).getType() == Token.Type.SEPARATOR) {
+                tokenizer.consume(); // consume separator
+            }
+        }
+
+        ParserUtil.checkType(tokenizer.consume(), Token.Type.LIST_END);
+
+
+        return new ListNode(elements, op.getPosition());
     }
 
     public static ListRule getInstance() {
