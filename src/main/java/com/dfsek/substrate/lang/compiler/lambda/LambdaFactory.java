f451ee3e1d9d6b8c4729a6a540974190c3137268
dfsek <dfsek@protonmail.com>
2021-10-31 10:06:35 -0700

    clean up LambdaFactory

--- src/main/java/com/dfsek/substrate/lang/compiler/lambda/LambdaFactory.java
+++ src/main/java/com/dfsek/substrate/lang/compiler/lambda/LambdaFactory.java
@@ -17,12 +17,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiConsumer;
 
 import static org.objectweb.asm.Opcodes.*;
 
 public class LambdaFactory {
-    private final Map<Signature, Map<Signature, Pair<Class<?>, List<Class<?>>>>> generated = new HashMap<>();
+    private final Map<Signature, Map<Signature, Pair<Class<?>, AtomicInteger>>> generated = new HashMap<>();
 
     private final DynamicClassLoader classLoader;
     private final TupleFactory tupleFactory;
@@ -65,17 +66,17 @@ public class LambdaFactory {
             Class<?> clazz = classLoader.defineClass(name.replace('/', '.'), bytes);
             CompilerUtil.dump(clazz, bytes);
 
-            return Pair.of(clazz, new ArrayList<>());
+            return Pair.of(clazz, new AtomicInteger(0));
         }).getLeft();
     }
 
     public Class<?> implement(Signature args, Signature returnType, BiConsumer<MethodVisitor, ClassWriter> consumer) {
         generate(args, returnType);
 
-        Pair<Class<?>, List<Class<?>>> pair = generated.get(args).get(returnType);
+        Pair<Class<?>, AtomicInteger> pair = generated.get(args).get(returnType);
 
         ClassWriter writer = new ClassWriter(org.objectweb.asm.ClassWriter.COMPUTE_FRAMES + org.objectweb.asm.ClassWriter.COMPUTE_MAXS);
-        String name = CompilerUtil.internalName(Lambda.class) + "IMPL_" + args.classDescriptor() + "$R" + returnType.classDescriptor() + "$IM" + pair.getRight().size();
+        String name = CompilerUtil.internalName(Lambda.class) + "IMPL_" + args.classDescriptor() + "$R" + returnType.classDescriptor() + "$IM" + pair.getRight().getAndIncrement();
 
         writer.visit(V1_8,
                 ACC_PUBLIC,
@@ -114,9 +115,6 @@ public class LambdaFactory {
                 null,
                 null);
 
-        int size = pair.getRight().size();
-        pair.getRight().add(null); // temp value
-
         consumer.accept(apply, writer);
         apply.visitMaxs(0, 0);
 
@@ -125,7 +123,6 @@ public class LambdaFactory {
 
         Class<?> clazz = classLoader.defineClass(name.replace('/', '.'), bytes);
         CompilerUtil.dump(clazz, bytes);
-        pair.getRight().set(size, clazz);
         return clazz;
     }
 }
