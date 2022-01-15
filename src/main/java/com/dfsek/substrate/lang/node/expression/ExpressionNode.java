b46baf0073e8eac886b70f0d22c48f9a505e2893
dfsek <dfsek@protonmail.com>
2022-01-14 20:19:18 -0700

    cache contents

--- src/main/java/com/dfsek/substrate/lang/node/expression/ExpressionNode.java
+++ src/main/java/com/dfsek/substrate/lang/node/expression/ExpressionNode.java
@@ -5,6 +5,7 @@ import com.dfsek.substrate.lang.compiler.build.BuildData;
 import com.dfsek.substrate.lang.compiler.codegen.ops.MethodBuilder;
 import com.dfsek.substrate.lang.compiler.type.Signature;
 import com.dfsek.substrate.lang.compiler.type.Typed;
+import com.dfsek.substrate.util.Lazy;
 import org.objectweb.asm.MethodVisitor;
 
 import java.util.Collection;
@@ -19,13 +20,18 @@ public abstract class ExpressionNode implements Node, Typed {
 
     public abstract Collection<? extends Node> contents();
 
+    private final Lazy<Collection<? extends Node>> cachedContents = Lazy.of(this::contents);
+
     public Stream<? extends Node> streamContents() {
         return streamContents(this);
     }
 
     private Stream<? extends Node> streamContents(Node start) {
         if (start instanceof ExpressionNode) {
-            return ((ExpressionNode) start).contents().stream().flatMap(node -> Stream.concat(Stream.of(node), streamContents(node)));
+            return ((ExpressionNode) start)
+                    .cachedContents.get()
+                    .stream()
+                    .flatMap(node -> Stream.concat(Stream.of(node), streamContents(node)));
         } else {
             return Stream.empty();
         }
