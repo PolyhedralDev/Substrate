7dbad0b8957082ff78f91594bc7de7a49e54d088
dfsek <dfsek@protonmail.com>
2022-02-12 22:32:04 -0700

    add assertion api

--- src/main/java/com/dfsek/substrate/lang/node/expression/ExpressionNode.java
+++ src/main/java/com/dfsek/substrate/lang/node/expression/ExpressionNode.java
@@ -1,10 +1,23 @@
 package com.dfsek.substrate.lang.node.expression;
 
+import com.dfsek.substrate.lang.compiler.type.Signature;
 import com.dfsek.substrate.lang.compiler.type.Typed;
+import com.dfsek.substrate.util.pair.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public abstract class ExpressionNode extends NodeHolder implements Typed {
+    private final List<Pair<ExpressionNode, Set<Signature>>> assertions = new ArrayList<>();
+
     @Override
     public ExpressionNode simplify() {
+        return this;
+    }
+
+    public ExpressionNode assertType(ExpressionNode node, Signature... signatures) {
+        assertions.add(Pair.of(node, Set.of(signatures)));
         return this;
     }
 }
