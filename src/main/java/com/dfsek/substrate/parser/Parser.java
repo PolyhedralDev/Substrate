730cf59907886d56ab874dfa03c5518d6c6c477e
dfsek <dfsek@protonmail.com>
2022-02-23 08:26:12 -0700

    dont reinstantiate static functions

--- src/main/java/com/dfsek/substrate/parser/Parser.java
+++ src/main/java/com/dfsek/substrate/parser/Parser.java
@@ -11,8 +11,11 @@ import com.dfsek.substrate.lang.std.function.Curry;
 import com.dfsek.substrate.lang.std.function.ForEach;
 import com.dfsek.substrate.lang.std.function.Println;
 import com.dfsek.substrate.lang.std.function.StaticFunction;
-import com.dfsek.substrate.parser.exception.ParseException;
 import com.dfsek.substrate.lexer.Lexer;
+import com.dfsek.substrate.parser.exception.ParseException;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Parser {
     private final Lexer lexer;
@@ -22,31 +25,38 @@ public class Parser {
 
     private final ParseData data = new ParseData();
 
+    private static final Map<String, StaticFunction> STATIC_FUNCTIONS = new LinkedHashMap<>();
+
+    static {
+        try {
+            STATIC_FUNCTIONS.put("pow", new StaticFunction(Math.class.getMethod("pow", double.class, double.class)));
+            STATIC_FUNCTIONS.put("sqrt", new StaticFunction(Math.class.getMethod("sqrt", double.class)));
+            STATIC_FUNCTIONS.put("cbrt", new StaticFunction(Math.class.getMethod("cbrt", double.class)));
+
+            STATIC_FUNCTIONS.put("abs", new StaticFunction(Math.class.getMethod("abs", int.class)));
+
+            STATIC_FUNCTIONS.put("sin", new StaticFunction(Math.class.getMethod("sin", double.class)));
+            STATIC_FUNCTIONS.put("cos", new StaticFunction(Math.class.getMethod("cos", double.class)));
+            STATIC_FUNCTIONS.put("tan", new StaticFunction(Math.class.getMethod("tan", double.class)));
+
+
+            STATIC_FUNCTIONS.put("substring", new StaticFunction(StringUtils.class.getMethod("substring", String.class, int.class, int.class)));
+            STATIC_FUNCTIONS.put("upperCase", new StaticFunction(StringUtils.class.getMethod("toUpperCase", String.class)));
+            STATIC_FUNCTIONS.put("lowerCase", new StaticFunction(StringUtils.class.getMethod("toLowerCase", String.class)));
+            STATIC_FUNCTIONS.put("startsWith", new StaticFunction(StringUtils.class.getMethod("startsWith", String.class, String.class)));
+            STATIC_FUNCTIONS.put("endsWith", new StaticFunction(StringUtils.class.getMethod("endsWith", String.class, String.class)));
+        } catch (NoSuchMethodException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
     public Parser(String data, Rule base) throws ParseException {
         lexer = new Lexer(data);
         registerFunction("println", new Println());
         registerMacro("forEach", new ForEach());
         registerMacro("curry", new Curry());
-        try {
-            registerFunction("pow", new StaticFunction(Math.class.getMethod("pow", double.class, double.class)));
-            registerFunction("sqrt", new StaticFunction(Math.class.getMethod("sqrt", double.class)));
-            registerFunction("cbrt", new StaticFunction(Math.class.getMethod("cbrt", double.class)));
 
-            registerFunction("abs", new StaticFunction(Math.class.getMethod("abs", int.class)));
-
-            registerFunction("sin", new StaticFunction(Math.class.getMethod("sin", double.class)));
-            registerFunction("cos", new StaticFunction(Math.class.getMethod("cos", double.class)));
-            registerFunction("tan", new StaticFunction(Math.class.getMethod("tan", double.class)));
-
-
-            registerFunction("substring", new StaticFunction(StringUtils.class.getMethod("substring", String.class, int.class, int.class)));
-            registerFunction("upperCase", new StaticFunction(StringUtils.class.getMethod("toUpperCase", String.class)));
-            registerFunction("lowerCase", new StaticFunction(StringUtils.class.getMethod("toLowerCase", String.class)));
-            registerFunction("startsWith", new StaticFunction(StringUtils.class.getMethod("startsWith", String.class, String.class)));
-            registerFunction("endsWith", new StaticFunction(StringUtils.class.getMethod("endsWith", String.class, String.class)));
-        } catch (NoSuchMethodException e) {
-            throw new IllegalStateException(e);
-        }
+        STATIC_FUNCTIONS.forEach(this::registerFunction);
 
         this.base = base;
     }
