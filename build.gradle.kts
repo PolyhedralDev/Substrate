7b4e6b8520c9764285dda6ea17d884a988f9301e
dfsek <dfsek@protonmail.com>
2022-03-08 08:23:29 -0700

    update libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    implementation("commons-io:commons-io:2.8.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    implementation("commons-io:commons-io:2.11.0")
     implementation("net.jafama:jafama:2.3.2")
     implementation("org.ow2.asm:asm:9.2")
 
@@ -26,6 +26,6 @@ java {
     targetCompatibility = JavaVersion.VERSION_11
 }
 
-tasks.withType<Test>() {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
