7f8ca769f3fcbc056e3380f2ef540e6da82c2343
dfsek <dfsek@protonmail.com>
2022-03-08 07:53:40 -0700

    remove unused OpCode interface

--- src/main/java/com/dfsek/substrate/lang/compiler/codegen/ops/OpCode.java
+++
@@ -1,8 +0,0 @@
-package com.dfsek.substrate.lang.compiler.codegen.ops;
-
-import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
-
-public interface OpCode extends Opcodes {
-    void generate(MethodVisitor visitor);
-}
