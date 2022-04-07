2fa156cb5969a1826a6bd39e224dd1dc09d9af4c
dfsek <dfsek@protonmail.com>
2022-04-07 15:55:13 -0700

    create compareInputsEquals test

--- src/test/java/IOTests.java
+++ src/test/java/IOTests.java
@@ -1,11 +1,44 @@
+import com.dfsek.substrate.lang.std.function.StaticFunction;
+import com.dfsek.substrate.parser.Parser;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.Charset;
 
 /**
  * Test input/output from scripts.
  */
 public class IOTests {
+    private static final String returnInputScript = getScript("returnInput");
+    private static final String compareInputsEquals = getScript("compareInputsEquals");
+
     @Test
     public void testSingleBooleanRecordInput() {
 
+    }
+
+    private static String getScript(String script) {
+        try {
+            return IOUtils.resourceToString("scripts/io/" + script + ".sbsc", Charset.defaultCharset());
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public class Records {
+        public record BooleanInput(boolean input) {
+        }
+
+        public record DoubleInput(double input) {
+        }
+
+        public record StringInput(String input) {
+        }
+
+        public record IntInput(int input) {
+        }
     }
 }
