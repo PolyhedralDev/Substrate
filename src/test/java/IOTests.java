d4f33ea378277d0e52de13443a49e83051366fbf
dfsek <dfsek@protonmail.com>
2022-04-07 16:01:11 -0700

    create primitive IOTests

--- src/test/java/IOTests.java
+++ src/test/java/IOTests.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.charset.Charset;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * Test input/output from scripts.
  */
@@ -16,13 +19,28 @@ public class IOTests {
     private static final String compareInputsEquals = getScript("compareInputsEquals");
 
     @Test
-    public void testSingleBooleanRecordInput() {
+    public void testSingleBooleanRecordInput() throws NoSuchMethodException {
+        assertTrue(Utils.createParser(returnInputScript, Records.BooleanInput.class, Records.BooleanInput.class).parse().execute(new Records.BooleanInput(true), null).input());
+    }
 
+    @Test
+    public void testSingleIntRecordInput() throws NoSuchMethodException {
+        assertEquals(5, Utils.createParser(returnInputScript, Records.IntInput.class, Records.IntInput.class).parse().execute(new Records.IntInput(5), null).input());
+    }
+
+    @Test
+    public void testSingleDoubleRecordInput() throws NoSuchMethodException {
+        assertEquals(5.5, Utils.createParser(returnInputScript, Records.DoubleInput.class, Records.DoubleInput.class).parse().execute(new Records.DoubleInput(5.5), null).input());
+    }
+
+    @Test
+    public void testSingleStringRecordInput() throws NoSuchMethodException {
+        assertEquals("bazinga", Utils.createParser(returnInputScript, Records.StringInput.class, Records.StringInput.class).parse().execute(new Records.StringInput("bazinga"), null).input());
     }
 
     private static String getScript(String script) {
         try {
-            return IOUtils.resourceToString("scripts/io/" + script + ".sbsc", Charset.defaultCharset());
+            return IOUtils.resourceToString("/scripts/io/" + script + ".sbsc", Charset.defaultCharset());
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
