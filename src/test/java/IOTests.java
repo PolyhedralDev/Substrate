dddd4f7f5cf4125f5f33405d4deef1ea0af6d269
dfsek <dfsek@protonmail.com>
2022-04-07 16:09:41 -0700

    add complex return test

--- src/test/java/IOTests.java
+++ src/test/java/IOTests.java
@@ -14,40 +14,46 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class IOTests {
     private static final String returnInputScript = getScript("returnInput");
     private static final String compareInputsEquals = getScript("compareInputsEquals");
+    private static final String returnTupleIntDoubleString = getScript("returnTupleIntDoubleString");
 
     @Test
-    public void testSingleBooleanRecordInput() throws NoSuchMethodException {
+    public void singleBooleanRecordInput() throws NoSuchMethodException {
         assertTrue(Utils.createParser(returnInputScript, Records.BooleanInput.class, Records.BooleanInput.class).parse().execute(new Records.BooleanInput(true), null).input());
     }
 
     @Test
-    public void testSingleIntRecordInput() throws NoSuchMethodException {
+    public void singleIntRecordInput() throws NoSuchMethodException {
         assertEquals(5, Utils.createParser(returnInputScript, Records.IntInput.class, Records.IntInput.class).parse().execute(new Records.IntInput(5), null).input());
     }
 
     @Test
-    public void testSingleDoubleRecordInput() throws NoSuchMethodException {
+    public void singleDoubleRecordInput() throws NoSuchMethodException {
         assertEquals(5.5, Utils.createParser(returnInputScript, Records.DoubleInput.class, Records.DoubleInput.class).parse().execute(new Records.DoubleInput(5.5), null).input());
     }
 
     @Test
-    public void testSingleStringRecordInput() throws NoSuchMethodException {
+    public void singleStringRecordInput() throws NoSuchMethodException {
         assertEquals("bazinga", Utils.createParser(returnInputScript, Records.StringInput.class, Records.StringInput.class).parse().execute(new Records.StringInput("bazinga"), null).input());
     }
 
     @Test
-    public void testIntEquals() throws NoSuchMethodException {
+    public void intEquals() throws NoSuchMethodException {
         assertTrue(Utils.createParser(compareInputsEquals, Records.TwoInts.class, Records.BooleanInput.class).parse().execute(new Records.TwoInts(5, 5), null).input());
     }
 
     @Test
-    public void testDoubleEquals() throws NoSuchMethodException {
+    public void doubleEquals() throws NoSuchMethodException {
         assertTrue(Utils.createParser(compareInputsEquals, Records.TwoDoubles.class, Records.BooleanInput.class).parse().execute(new Records.TwoDoubles(5.5, 5.5), null).input());
     }
 
     @Test
-    public void testStringEquals() throws NoSuchMethodException {
+    public void stringEquals() throws NoSuchMethodException {
         assertTrue(Utils.createParser(compareInputsEquals, Records.TwoStrings.class, Records.BooleanInput.class).parse().execute(new Records.TwoStrings("bazinga", "bazinga"), null).input());
+    }
+
+    @Test
+    public void returnTupleIntDoubleString() throws NoSuchMethodException {
+        assertEquals(new Records.IntDoubleString(5, 4.3, "bazinga"), Utils.createParser(returnTupleIntDoubleString, Records.Void.class, Records.IntDoubleString.class).parse().execute(new Records.Void(), null));
     }
 
     private static String getScript(String script) {
@@ -79,6 +85,12 @@ public class IOTests {
         }
 
         public record TwoInts(int input1, int input2) {
+        }
+
+        public record IntDoubleString(int input1, double input2, String input3) {
+        }
+
+        public record Void() {
         }
     }
 }
