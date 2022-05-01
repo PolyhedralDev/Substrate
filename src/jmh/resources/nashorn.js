84c0f132d216210ce87551f5d4e01a64fe6569f2
dfsek <dfsek@protonmail.com>
2022-05-01 16:23:57 -0700

    clean up performance scripts

--- src/jmh/resources/nashorn.js
+++ src/jmh/resources/nashorn.js
@@ -1,16 +1,11 @@
 var epsilon = 0.001;
 
 var x = input;
-
 var root = 0;
-
 while (true) {
     root = 0.5 * (x + (input / x));
-
     var diff = Math.abs(root - x);
-
     if (diff < epsilon) break;
-
     x = root;
 }
 
