3451c2501fc66dfb5ccd71b7b2b85b0904afaad5
dfsek <dfsek@protonmail.com>
2022-05-13 18:28:29 -0700

    implement Unchecked.Folded

--- src/main/java/com/dfsek/substrate/lang/compiler/type/Unchecked.java
+++ src/main/java/com/dfsek/substrate/lang/compiler/type/Unchecked.java
@@ -1,11 +1,13 @@
 package com.dfsek.substrate.lang.compiler.type;
 
-import com.dfsek.substrate.parser.exception.ParseException;
+import com.dfsek.substrate.lang.node.expression.ExpressionNode;
+import com.dfsek.substrate.lang.node.expression.error.ErrorNode;
 import io.vavr.control.Either;
 
 import java.util.Arrays;
+import java.util.function.Function;
 
-public class Unchecked<T extends Typed> {
+public final class Unchecked<T extends Typed> implements Typed{
     private Unchecked(T value) {
         this.value = value;
     }
@@ -20,5 +22,35 @@ public class Unchecked<T extends Typed> {
         Signature ref = value.reference();
         for (Signature type : expected) if (ref.equals(type)) return Either.right(value);
         return Either.left("Expected type(s) " + Arrays.toString(expected) + " but found " + ref);
+    }
+
+    public static <T extends ExpressionNode> Folded<T> fold(Unchecked<T> unchecked) {
+        return new Folded<>(unchecked);
+    }
+
+    @Override
+    public Signature reference() {
+        return value.reference();
+    }
+
+    public static final class Folded<T extends ExpressionNode> implements Typed {
+        private final Unchecked<T> value;
+
+        private Folded(Unchecked<T> value) {
+            this.value = value;
+        }
+
+        public ExpressionNode get(Signature... signatures) {
+            return value.get(signatures)
+                    .fold(
+                            err -> new ErrorNode(value.value.getPosition(), err),
+                            Function.identity()
+                    );
+        }
+
+        @Override
+        public Signature reference() {
+            return value.value.reference();
+        }
     }
 }
