f6c22a8ad0418af54bf47928a7b76ff089190d57
dfsek <dfsek@protonmail.com>
2022-05-17 08:58:37 -0700

    add IO#bind

--- src/main/java/com/dfsek/substrate/environment/IO.java
+++ src/main/java/com/dfsek/substrate/environment/IO.java
@@ -1,5 +1,11 @@
 package com.dfsek.substrate.environment;
 
+import java.util.function.Function;
+
 public interface IO<V, T extends Environment> {
     V apply(T env);
+
+    static <T, U, E extends Environment> IO<U, E> bind(IO<T, E> io, Function<T, IO<U, E>> function) {
+        return env -> function.apply(io.apply(env)).apply(env);
+    }
 }
