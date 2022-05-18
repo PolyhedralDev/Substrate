938555b2e5eb92ed5f4c49f6cb6e34bfd43e38fb
dfsek <dfsek@protonmail.com>
2022-05-17 21:22:48 -0700

    create IOFunctionUnit2Obj

---
+++ src/main/java/com/dfsek/substrate/environment/io/IOFunctionUnit2Obj.java
@@ -0,0 +1,7 @@
+package com.dfsek.substrate.environment.io;
+
+import com.dfsek.substrate.environment.Environment;
+
+public interface IOFunctionUnit2Obj<B> {
+    B apply(Environment environment);
+}
